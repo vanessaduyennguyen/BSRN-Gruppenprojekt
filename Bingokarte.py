c868fde70eecec4b50eae72c0bbbdc42ad201789
Vanessa Nguyen <vanessanguyen33@gmail.com>
2024-05-18 23:09:27 +0200

    Update Bingokarte.py

--- Bingokarte.py
+++ Bingokarte.py
@@ -11,12 +11,11 @@ gridLayout = ttk.TTkGridLayout(columnMinHeight=0,columnMinWidth=0)
     # Hauptwidget wird erstellt (mit dem zuvor erstellten TTkGridLayout-Layout initialisiert): Ein neues TTk-Objekt wird erstellt
 root = ttk.TTk(layout=gridLayout)
 
+# Bingokarte wird erstellt 
 for x in range(0, felder_Anzahl):
-    feld = ttk.TTkButton(parent=root, border=True, text="Button R1 " + str (x+1))
-    
-    for y in range(1, felder_Anzahl):
-        feld = ttk.TTkButton(parent=root, border=True, text="Button R2 " + str (x+1))
-        gridLayout.addWidget(feld, y, x)
+    for y in range(0, felder_Anzahl):
+        feld = ttk.TTkButton(parent=root, border=True, text="Button R " + str (x+1))
+        gridLayout.addWidget(feld, row=y, col=x, rowspan=1, colspan=1, direction=3)
        
         if felder_Anzahl == 5 and y == 2 and x == 2 or felder_Anzahl == 7 and y == 3 and x == 3 :
             ersetze_ButtonText(feld)
