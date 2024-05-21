88760be9ab5d63f39be6ea30f6bc568eddbbca6e
Vanessa Nguyen <vanessanguyen33@gmail.com>
2024-05-21 20:10:39 +0200

    Update BingokarteTextdatei.py

--- BingokarteTextdatei.py
+++ BingokarteTextdatei.py
@@ -5,6 +5,9 @@ import random
 
 def JOKER_ausfüllen(feld):
     feld.setText("JOKER")
+    feld.setChecked(True)
+    return feld
+
 
 def open_file(filename):
     with open(filename, 'r') as file:
@@ -14,17 +17,25 @@ def create_bingo_card(felder_Anzahl, words):
     gridLayout = ttk.TTkGridLayout(columnMinHeight=0,columnMinWidth=0)
     root = ttk.TTk(layout=gridLayout)
 
+    felder_matrix = []
     for x in range(felder_Anzahl):
+        rows = []
         for y in range(felder_Anzahl):
+  
             word = random.choice(words)
             words.remove(word)
-            feld = ttk.TTkButton(parent=root, border=True, text=word)
+            feld = ttk.TTkButton(parent=root, border=True, text=word, checked = False, checkable = True)
             gridLayout.addWidget(feld, row=y, col=x, rowspan=1, colspan=1, direction=3)
-            
-            if felder_Anzahl == 5 and y == 2 and x == 2 or felder_Anzahl == 7 and y == 3 and x == 3:
-                JOKER_ausfüllen(feld)
+            rows.append(feld)
 
+            if felder_Anzahl == 5 and y == 2 and x == 2 or felder_Anzahl == 7 and y == 3 and x == 3:
+                neues_Feld = JOKER_ausfüllen(feld)
+                last_index = len(rows)-1
+                rows[last_index] = neues_Feld
+        felder_matrix.append(rows)
+        
     root.mainloop()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="textdateibefehl.py", add_help=False)
@@ -42,3 +53,4 @@ if __name__ == "__main__":
         create_bingo_card(felder_Anzahl, words)
     else:
         print(f"Die Datei {filename} existiert nicht.")
+
