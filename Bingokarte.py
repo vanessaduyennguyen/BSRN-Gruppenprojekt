7f5d49329019829360964285ac3abf0fa0f1a469
Vanessa Nguyen <vanessanguyen33@gmail.com>
2024-05-21 17:10:02 +0200

    Update Bingokarte.py

--- Bingokarte.py
+++ Bingokarte.py
@@ -14,7 +14,7 @@ root = ttk.TTk(layout=gridLayout)
 # Bingokarte wird erstellt 
 for x in range(0, felder_Anzahl):
     for y in range(0, felder_Anzahl):
-        feld = ttk.TTkButton(parent=root, border=True, text="Button R " + str (x+1))
+        feld = ttk.TTkButton(parent=root, border=True, text="Button R " + str (x+1), checked = False, checkable = True)
         gridLayout.addWidget(feld, row=y, col=x, rowspan=1, colspan=1, direction=3)
        
         if felder_Anzahl == 5 and y == 2 and x == 2 or felder_Anzahl == 7 and y == 3 and x == 3 :
