3b46462e25b9418fe2112891c15f449e3d81d04d
Vanessa Nguyen <vanessanguyen33@gmail.com>
2024-05-21 20:29:41 +0200

    Update BingokarteTextdatei.py

--- BingokarteTextdatei.py
+++ BingokarteTextdatei.py
@@ -3,6 +3,8 @@ import argparse
 import os
 import random
 
+# Die Methode soll das Feld in der Mitte ersetzen und automatisch markieren.
+# Das Feld mit den neuen Eigenschaften wird zurückgegeben.
 def JOKER_ausfüllen(feld):
     feld.setText("JOKER")
     feld.setChecked(True)
@@ -16,24 +18,29 @@ def open_file(filename):
 def create_bingo_card(felder_Anzahl, words):
     gridLayout = ttk.TTkGridLayout(columnMinHeight=0,columnMinWidth=0)
     root = ttk.TTk(layout=gridLayout)
-
+    # Eine 2D-Liste (Matrix von den Bingofeldern) wird zur Überprüfung, ob es ein Bingo gibt, erstellt.
     felder_matrix = []
     for x in range(felder_Anzahl):
+        # Es wird eine Reihe der Matrix erstellt
         rows = []
         for y in range(felder_Anzahl):
-  
             word = random.choice(words)
             words.remove(word)
             feld = ttk.TTkButton(parent=root, border=True, text=word, checked = False, checkable = True)
-            gridLayout.addWidget(feld, row=y, col=x, rowspan=1, colspan=1, direction=3)
+            gridLayout.addWidget(feld, row = x, col = y, rowspan=1, colspan=1, direction=3)
+            # Zu der Reihe der Matrix werden bei jedem Durchlauf der Schleife TTkButton-Objekte hinzugefügt (insgesamt felder_Anzahl-Objekte).
             rows.append(feld)
 
             if felder_Anzahl == 5 and y == 2 and x == 2 or felder_Anzahl == 7 and y == 3 and x == 3:
                 neues_Feld = JOKER_ausfüllen(feld)
+                # Der Index von dem altem Feld(der zuletzt genutzte Index) wird durch das neue Feld (mit den "neuen Attributen") ersetzt.
                 last_index = len(rows)-1
                 rows[last_index] = neues_Feld
+
+        # Die "fertige" Reihe wird nun der Matrix hinzugefügt
+        # Bei erneuten Durchlauf der Schleife wird wieder eine neue Reihe erstellt, hinzugefügt, etc....
         felder_matrix.append(rows)
-        
+
     root.mainloop()
 
 
